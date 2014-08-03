6deca4cb8053bff682f72d83c9e7ee8196ee04c7
Bradon Kanyid <rattboi@gmail.com>
2014-08-02 19:15:18 -0700

    Added flags/arguments. Refactored TCP part to own function

--- build-watcher.go
+++ build-watcher.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "flag"
     "log"
     "strings"
     "net"
@@ -9,7 +10,63 @@ import (
     "github.com/ActiveState/tail"
 )
 
+var botname string
+var botaddress string
+var botport string
+var ircchan string
+var authpass string
+var watchdir string
+
+func init() {
+    flag.StringVar(&botname,    "ircbot", "ircflu", "irc bot name")
+    flag.StringVar(&botaddress, "botaddress", "localhost", "address where ircflu cat server is")
+    flag.StringVar(&botport,    "botport", "12345", "port where ircflu cat server is")
+    flag.StringVar(&ircchan,    "ircchan", "", "channel to join after authing the bot")
+    flag.StringVar(&authpass,   "authpass", "", "password to auth the bot")
+    flag.StringVar(&watchdir,   "watchdir", "/tmp", "directory to watch for build files")
+
+}
+
+func WriteToIrcBot(message string) {
+    strEcho := message + "\n"
+    servAddr := botaddress + ":" + botport
+    tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+
+    if err != nil {
+        println("ResolveTCPAddr failed:", err.Error())
+        os.Exit(1)
+    }
+
+    conn, err := net.DialTCP("tcp", nil, tcpAddr)
+    if err != nil {
+        println("Dial failed:", err.Error())
+        os.Exit(1)
+    }
+
+    _, err = conn.Write([]byte(strEcho))
+
+    if err != nil {
+        println("Write to server failed:", err.Error())
+        return
+    }
+    conn.Close()
+}
+
 func main() {
+    // parse out flags
+    flag.Parse()
+
+    // a few arguments don't have defaults
+    if ircchan == "" {
+        println("Must supply ircchan argument")
+        os.Exit(1);
+    }
+
+    if authpass == "" {
+        println("Must supply authpass argument")
+        os.Exit(1);
+    }
+
     watcher, err := fsnotify.NewWatcher()
     if err != nil {
         log.Fatal(err)
@@ -24,38 +81,19 @@ func main() {
             case ev := <-watcher.Event:
                 // See if a new file is created with build- in the name
                 if strings.Contains(ev.Name,"build-") && ev.IsCreate() {
-                    log.Println("file created. Name -> ", ev.Name)
+                    log.Println("New File -> ", ev.Name)
                     // 'fork' a new tail watcher for this build file
                     go func() {
                         t, err := tail.TailFile(ev.Name, tail.Config{Follow: true})
                         if err != nil {
                             return
                         }
+
+                        WriteToIrcBot("@" + botname + " !auth " + authpass)
+                        WriteToIrcBot("@" + botname + " !join #" + ircchan)
+
                         for line := range t.Lines {
-                            strEcho := line.Text + "\n"
-                            servAddr := "localhost:12345"
-                            tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-                            if err != nil {
-                                println("ResolveTCPAddr failed:", err.Error())
-                                os.Exit(1)
-                            }
-
-                            conn, err := net.DialTCP("tcp", nil, tcpAddr)
-                            if err != nil {
-                                println("Dial failed:", err.Error())
-                                os.Exit(1)
-                            }
-
-                            _, err = conn.Write([]byte(strEcho))
-                            if err != nil {
-                                println("Write to server failed:", err.Error())
-                                os.Exit(1)
-                            }
-
-                            println("write to server = ", strEcho)
-
-                            conn.Close()
-
+                            WriteToIrcBot("#" + ircchan + " " + line.Text)
                             log.Println(line.Text)
                         }
                     }()
@@ -66,8 +104,7 @@ func main() {
         }
     }()
 
-    // Watching /tmp for now. Need to parameterize
-    err = watcher.Watch("/tmp")
+    err = watcher.Watch(watchdir)
     if err != nil {
         log.Fatal(err)
     }
