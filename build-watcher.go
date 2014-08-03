18c839dc9f0becffdfe915086da22302e4d63ba0
Bradon Kanyid <rattboi@gmail.com>
2014-08-02 17:40:41 -0700

    Added .gitignore. Now forwards log info to tcp port. Lots of hard-coding so far

--- build-watcher.go
+++ build-watcher.go
@@ -3,6 +3,8 @@ package main
 import (
     "log"
     "strings"
+    "net"
+    "os"
     "github.com/howeyc/fsnotify"
     "github.com/ActiveState/tail"
 )
@@ -19,22 +21,47 @@ func main() {
     go func() {
         for {
             select {
-                case ev := <-watcher.Event:
-                    // See if a new file is created with build- in the name
-                    if strings.Contains(ev.Name,"build-") && ev.IsCreate() {
-                        log.Println("file created. Name -> ", ev.Name)
-                        // 'fork' a new tail watcher for this build file
-                        go func() {
-                            t, err := tail.TailFile(ev.Name, tail.Config{Follow: true})
-                            if err == nil {
-                                for line := range t.Lines {
-                                        log.Println(line.Text)
-                                }
+            case ev := <-watcher.Event:
+                // See if a new file is created with build- in the name
+                if strings.Contains(ev.Name,"build-") && ev.IsCreate() {
+                    log.Println("file created. Name -> ", ev.Name)
+                    // 'fork' a new tail watcher for this build file
+                    go func() {
+                        t, err := tail.TailFile(ev.Name, tail.Config{Follow: true})
+                        if err != nil {
+                            return
+                        }
+                        for line := range t.Lines {
+                            strEcho := line.Text + "\n"
+                            servAddr := "localhost:12345"
+                            tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+                            if err != nil {
+                                println("ResolveTCPAddr failed:", err.Error())
+                                os.Exit(1)
                             }
-                        }()
-                    }
-                case err := <-watcher.Error:
-                    log.Println("error:", err)
+
+                            conn, err := net.DialTCP("tcp", nil, tcpAddr)
+                            if err != nil {
+                                println("Dial failed:", err.Error())
+                                os.Exit(1)
+                            }
+
+                            _, err = conn.Write([]byte(strEcho))
+                            if err != nil {
+                                println("Write to server failed:", err.Error())
+                                os.Exit(1)
+                            }
+
+                            println("write to server = ", strEcho)
+
+                            conn.Close()
+
+                            log.Println(line.Text)
+                        }
+                    }()
+                }
+            case err := <-watcher.Error:
+                log.Println("error:", err)
             }
         }
     }()
