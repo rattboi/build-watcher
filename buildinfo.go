757bc92edaadfc1acc5c7f8104deaf733a9d4e85
Bradon Kanyid <rattboi@gmail.com>
2014-09-25 12:58:56 -0700

    Added parsing of self-service deploys, and summarizing projects further

--- buildinfo.go
+++ buildinfo.go
@@ -29,20 +29,36 @@ func initBuildInfo(build *BuildInfo) {
 	}
 }
 
+func summarizeProject(projects string) string {
+	var parsedProjects = strings.Replace(projects, "EIT_1World_", "", -1)
+	parsedProjects = strings.Replace(parsedProjects, "EIT_1WORLD_", "", -1)
+	parsedProjects = strings.Replace(parsedProjects, "Service", "Svc", -1)
+	parsedProjects = strings.Replace(parsedProjects, "Invoice", "Inv", -1)
+	parsedProjects = strings.Replace(parsedProjects, "Entity", "Ent", -1)
+	parsedProjects = strings.Replace(parsedProjects, "Custom", "Cust", -1)
+	parsedProjects = strings.Replace(parsedProjects, "Operation", "Op", -1)
+	parsedProjects = strings.Replace(parsedProjects, "Process", "Proc", -1)
+	parsedProjects = strings.Replace(parsedProjects, "Preference", "Pref", -1)
+	parsedProjects = strings.Replace(parsedProjects, "Schedule", "Sched", -1)
+	return parsedProjects
+}
+
 func getBuildInfo(buildStatus string, buildInfo BuildInfo) string {
 	var info = buildInfo.Matches
 	switch buildStatus {
 	case "START":
 		{
-			var res []string
-			var envRE, _ = regexp.Compile(`Deploy to (.*) - DEPLOY ONE PROJECT.*`)
-			res = envRE.FindStringSubmatch(info["builddef"])
-			var parsedProjects = strings.Replace(info["projects"], "EIT_1World_", "", -1)
-			parsedProjects = strings.Replace(parsedProjects, "EIT_1WORLD_", "", -1)
-			if res != nil {
-				return fmt.Sprintf("%v: %v : %v : %v : %v ", hashedColor(info["buildlabel"]), pad(buildStatus, 7), pad(info["requestor"], 14), pad(res[1], 12), parsedProjects)
+			var res1, res2 []string
+			var batchEnvRE = regexp.MustCompile(`Deploy to (.*) - DEPLOY ONE PROJECT.*`)
+			var selfSEnvRE = regexp.MustCompile(`Dev DEPLOY (.*) - (.*)`)
+			res1 = batchEnvRE.FindStringSubmatch(info["builddef"])
+			res2 = selfSEnvRE.FindStringSubmatch(info["builddef"])
+			if res1 != nil {
+				return fmt.Sprintf("%v: %v : %v : %v : %v ", hashedColor(info["buildlabel"]), pad(buildStatus, 7), pad(info["requestor"], 14), pad(res1[1], 14), summarizeProject(info["projects"]))
+			} else if res2 != nil {
+				return fmt.Sprintf("%v: %v : %v : %v : %v ", hashedColor(info["buildlabel"]), pad(buildStatus, 7), pad(info["requestor"], 14), pad(res2[1], 14), summarizeProject(res2[2]))
 			} else {
-				return fmt.Sprintf("%v: %v : %v : %v : %v ", hashedColor(info["buildlabel"]), pad(buildStatus, 7), pad(info["requestor"], 14), info["builddef"], parsedProjects)
+				return fmt.Sprintf("%v: %v : %v : %v : %v ", hashedColor(info["buildlabel"]), pad(buildStatus, 7), pad(info["requestor"], 14), info["builddef"], summarizeProject(info["projects"]))
 			}
 		}
 	case "FAIL":
